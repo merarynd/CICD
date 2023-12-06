5399a02e0eb41361ca58603dede945abc08485dc
admin <admin@MacBook-Air-admin.local>
2023-12-06 17:41:10 +0300

    all

---
+++ src/script.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+scp /home/merarynd/builds/Lgd52JXQ/0/students/DO6_CICD.ID_356283/merarynd_student.21_school.ru/DO6_CICD-1/src/cat/s21_cat merarynd@10.10.0.2:/usr/local/bin
+scp /home/merarynd/builds/Lgd52JXQ/0/students/DO6_CICD.ID_356283/merarynd_student.21_school.ru/DO6_CICD-1/src/grep/s21_grep merarynd@10.10.0.2:/usr/local/bin
